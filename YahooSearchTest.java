1709ec43e34ede16c7b719c20cb5ef3d428a674b
Kalpana Palaniswamy <KPALI@FMSUZ.COM>
2018-10-06 17:33:34 -0400

    My first java file added in the repository

---
+++ YahooSearchTest.java
@@ -0,0 +1,39 @@
+package YahooSearch;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class YahooSearchTest {
+    private WebDriver ydriver;
+
+    @BeforeTest
+    public void setupDriver(){
+//C:\Users\kpalaniswamy\IdeaProjects\Webdriverbootcamp\src\test\resource\geckodriver.exe
+        File file = new File("src/test/resource");
+        String path = file.getAbsolutePath();
+        System.setProperty("webdriver.gecko.driver", path+"\\geckodriver.exe");
+        //System.out.println(path+"\\geckodriver.exe");
+        ydriver = new FirefoxDriver();
+    }
+    @Test
+    public void testSearch() throws Exception{
+        navigateToMainPage();
+        typeRequestInSearchInput();
+       /*submitSearch();
+        printAmountOfResult();*/
+    }
+    private void navigateToMainPage(){
+        String url = "https://yahoo.com";
+        ydriver.get(url);
+
+    }
+    private void typeRequestInSearchInput(){
+        ydriver.findElement(By.id("uh-search-box")).sendKeys("What is the weather today");
+    }
+
+}
